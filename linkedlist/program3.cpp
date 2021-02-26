a25a8280dc4c0cd0535bc372f5032759312d308d
lakshay2k <lakshaykmsharma@gmail.com>
2021-02-26 08:48:04 +0530

    How to reverse a linked list using C++ - I solved it using three pointer approach

---
+++ linkedlist/program3.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+       ListNode *prevnode=NULL, *currnode=head, *nextnode=head;
+        while(nextnode!=NULL)
+        {
+            nextnode=nextnode->next;
+            currnode->next=prevnode;
+            prevnode=currnode;
+            currnode=nextnode;
+        }
+        head=prevnode;
+        return head;
+    }
+};
