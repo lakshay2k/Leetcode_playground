6bc964af9250b071152511381b937096128ec375
lakshay2k <lakshaykmsharma@gmail.com>
2021-03-03 11:59:00 +0530

    Climbing Stairs using

---
+++ dynamic_programming/program1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+       int i;
+        vector<int> mysteps;
+        //if person comes from (n-1)th stair
+        mysteps.push_back(1); 
+        //if person comes from (n-2)th stair
+        mysteps.push_back(2);
+        
+        //counting number of ways to reach (n-1)th and (n-2)th stair
+        for(i=2;i<n;i++)
+        {
+            mysteps.push_back(mysteps[i-1]+mysteps[i-2]);
+        }
+        return mysteps[n-1];
+    }
+};
