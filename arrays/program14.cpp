b27a3293a074d5e9742d9287b2ce6f6db19adcc0
lakshay2k <lakshaykmsharma@gmail.com>
2021-03-17 11:18:24 +0530

    Longest Substring without repeating characters using c

---
+++ arrays/program14.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        // count the length of longest substring in ans
+        int ans=0;
+        int n=s.size();
+        unordered_set<char> myset;
+        //creating a window initially starting them both with zero
+        int l=0,r=0;
+        while(l<n&&r<n)
+        {
+            //if the set does not contain the aplhabet of string
+            if(myset.find(s[r])==myset.end())
+            {
+                myset.insert(s[r]);
+                //checking the length and increamenting if the window size increases
+                ans=max(ans,(r-l+1));
+                r++;
+            }
+            //first remove the alphabet and move the window forward.
+            else
+            {
+                myset.erase(s[l]);
+                l++;
+            }
+        }
+        
+        return ans;
+    }
+};
