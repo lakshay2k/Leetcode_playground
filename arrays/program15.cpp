98d2df83c3e997ec703e6597e55978945371d6da
lakshay2k <lakshaykmsharma@gmail.com>
2021-03-18 10:29:47 +0530

    Longest Palindrome Substring using C++

---
+++ arrays/program15.cpp
@@ -0,0 +1,45 @@
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        //1 length string is by default palindrome
+        int maxLength = 1;
+        string res;
+ 
+    int start = 0;
+    int len = s.size();
+ 
+    int low, high;
+    for (int i = 1; i < len; ++i) {
+        //for even size palindrome
+        //we will assign two adjacent values in low and high for even palindrome 
+        low = i - 1;
+        high = i;
+        while (low >= 0 && high < len && s[low] == s[high]) {
+            if (high - low + 1 > maxLength) {
+                start = low;
+                maxLength = high - low + 1;
+            }
+            --low;
+            ++high;
+        }
+
+        //for odd size palindrome
+        //we will assign in odd pairs to have a middle element
+        low = i - 1;
+        high = i + 1;
+        while (low >= 0 && high < len && s[low] == s[high]) {
+            if (high - low + 1 > maxLength) {
+                start = low;
+                maxLength = high - low + 1;
+            }
+            --low;
+            ++high;
+        }
+    }
+ res = s.substr(start,maxLength);
+        return res;
+    
+        
+    }
+};
