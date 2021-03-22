14fc57c4b8b563632a2d08c4a4386a341543b45b
lakshay2k <lakshaykmsharma@gmail.com>
2021-03-22 14:24:13 +0530

    Intersection of two linkedlist using C++

---
+++ linkedlist/program9.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        //Creating node for storing intersection node pointer
+        ListNode* ans = NULL;
+        //Making all the values negative connected by a
+       for(ListNode* a = headA;a!=NULL;a=a->next)
+       {
+           a->val = -(a->val);
+       }
+        //chosing first values that comes negative as it is the only intersection
+        for(ListNode* b = headB;b!=NULL;b=b->next)
+        {
+            if(b->val < 0)
+            {
+                //ans becomes first negative node
+                ans=b;
+                break;
+            }
+        }
+        //redefining the values as positive and returning the ans pointer.
+        for(ListNode* a = headA;a!=NULL;a=a->next)
+       {
+           a->val = -(a->val);
+       }
+        return ans;
+    }
+};
