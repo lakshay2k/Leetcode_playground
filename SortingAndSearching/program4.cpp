2d2bb89a4b98c3a11b79eb1c69d906a60d858558
lakshay2k <lakshaykmsharma@gmail.com>
2021-03-24 08:51:05 +0530

    Added the comments to Top K frequent elements in c++

--- SortingAndSearching/program4.cpp
+++ SortingAndSearching/program4.cpp
@@ -5,7 +5,9 @@ public:
     {
         return a.second>b.second;
     }
+
     vector<int> topKFrequent(vector<int>& nums, int k) {
+
         //creating hashmap to make key value pair having value and its occurences.
         unordered_map<int,int> mymap;
         for(int i=0;i<nums.size();i++)
@@ -13,10 +15,12 @@ public:
             //values of same key gets one incremented.
             mymap[nums[i]]++;
         }
+
         //creating pairs to sort based on the values / occurences i.e the second attribute
         vector<pair<int,int>> res(mymap.begin(),mymap.end());
         sort(res.begin(),res.end(),sortsecond);
         vector<int> ans;
+
         //storing back the first keys in the vector and returning it.
         for(int i=0;i<k;i++)
         {
