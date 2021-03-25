658f60b2e7374b6a8310efa0ebe993844905943b
lakshay2k <lakshaykmsharma@gmail.com>
2021-03-25 09:43:03 +0530

    finding kth largest element in vector using C++

---
+++ SortingAndSearching/program5.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        //sorting the element using library sort function
+        sort(nums.begin(),nums.end());
+        //and returning the kth element
+        return nums[nums.size()-k];
+        
+    }
+};
