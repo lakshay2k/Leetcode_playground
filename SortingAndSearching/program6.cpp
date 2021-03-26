2fd9e7e94dc63b4ce6b0ae8b56cf2199dfaa40bf
lakshay2k <lakshaykmsharma@gmail.com>
2021-03-26 08:51:53 +0530

    Finding the peak element in vectors using C++

---
+++ SortingAndSearching/program6.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int mypeakfunc(vector<int> nums, int low, int high)
+    {
+        //for logarithmic complexity, we will use binary search 
+        int n=nums.size();
+        int mid, l=low, r=high;
+        //calculatig the middle element
+            mid = l + (r-l)/2;
+        //if the mid elememt satisfies the conditions in question, then return mid
+             if((mid==0|| nums[mid]>nums[mid-1]) && (mid==n-1 ||nums[mid]>nums[mid+1]))
+                return mid;   
+        //else if element left to mid is greater, go for left part
+            else if(mid>0 && nums[mid-1]>nums[mid])
+            {
+                return mypeakfunc(nums,l,mid-1);
+            }
+        //otherwise go for right part
+            else
+            {
+                return mypeakfunc(nums,mid+1,r);
+            }
+    }
+    int findPeakElement(vector<int>& nums) {
+        //caling my peak function
+        return mypeakfunc(nums,0,nums.size()-1);
+    }
+};
