7767368897bed6ad9479823300b704194351f31b
lakshay2k <lakshaykmsharma@gmail.com>
2021-04-06 08:54:16 +0530

    Spiral matrix implemented in C++ - Comments added

--- arrays/program18.cpp
+++ arrays/program18.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         
         //creating four pointers to cover boundaries and go in matrix spirally
@@ -12,34 +13,43 @@ public:
         
         vector<int> res;
         
+//top should always be less than bottom , similarly left and right.
         while(top<=bottom && left<=right)
         {
+            //when we move left to right
             if(dir == 0)
             {
                 for(i=left;i<=right;i++)
                     res.push_back(matrix[top][i]);
                 top++;
             }
+            //when we move top to bottom
             else if(dir == 1)
             {
                 for(i=top;i<=bottom;i++)
                     res.push_back(matrix[i][right]);
                 right--;
             }
+            //when we move right to left
             else if(dir == 2)
             {
                 for(i=right;i>=left;i--)
                     res.push_back(matrix[bottom][i]);
                 bottom--;
             }
+            //when we move bottom to top
             else if(dir == 3)
             {
                 for(i=bottom;i>=top;i--)
                     res.push_back(matrix[i][left]);
                 left++;
             }
+
+           //changing the direction every while loop and its within bound of 4,
             dir = (dir+1)%4;
         }
+
+        //return result vector
         return res;
     }
 };
