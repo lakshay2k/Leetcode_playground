4d4f003f9549cacdee1be9621e6cbb73734a6084
lakshay2k <lakshaykmsharma@gmail.com>
2021-04-23 10:44:01 +0530

    FizzBuzz in C++

--- maths/program1.cpp
+++ maths/program1.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     vector<string> fizzBuzz(int n) {
+
         vector<string> res;
         
+//remeber to check for combined condition first and then the individual conditions
         for(int i=1;i<=n;i++)
         {
             if(i%3 == 0 && i%5 == 0)
@@ -14,6 +16,8 @@ public:
             else
                 res.push_back(to_string(i));
         }
+
+/return resultant vector
         return res;
     }
 };
