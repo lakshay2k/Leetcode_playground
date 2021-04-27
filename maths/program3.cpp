7c28560f9a15f987ae3ac861f6e304c655c861a9
lakshay2k <lakshaykmsharma@gmail.com>
2021-04-27 16:12:31 +0530

    Power of three - without recursion of loop in C++

---
+++ maths/program3.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        
+        if(n>1)
+        {
+            //we will factorise till we get one which states that number is power of 3 otherwise not
+            while(n%3 == 0)
+            {
+                n=n/3;
+            }
+        }
+        return n == 1;
+    }
+};
