13536f7646f508d5078750b5cd30f94a6a6e7f3d
lakshay2k <lakshaykmsharma@gmail.com>
2021-05-01 10:41:42 +0530

    Move Zeroes - optimal solution in C++ (comments added)

--- arrays/program27.cpp
+++ arrays/program27.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
+
     void moveZeroes(vector<int>& nums) {
         
+        //taking size in k variable
         int k=nums.size(),j=0,i;
+
         int num=count(nums.begin(), nums.end(), 0); //counting number of zeros in array
         
         for(i=0;i<k;i++){
