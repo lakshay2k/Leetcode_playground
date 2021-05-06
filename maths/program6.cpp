70baf64bcb11c8d46ac6f547760e1a89fa3250eb
lakshay2k <lakshaykmsharma@gmail.com>
2021-05-06 10:25:17 +0530

    Find numbers with even number of digits in C++ - comments added

--- maths/program6.cpp
+++ maths/program6.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
+
     int findNumbers(vector<int>& nums) {
         int i,rem=0,n=0,count=0,dig=0;
             for(i=0;i<nums.size();i++)
             {
                 n=nums[i];
+                //counting number of digits using divide
                 while(n>0)
                 {
                     n=n/10;
                     dig=dig+1;
                 }
+
+                //checking whether digits found are even or odd
                 if(dig%2==0)
                 {
                     count=count+1;
                 }
+
+                //for every new number we count the digits so we make digits to zero at last
                 dig=0;
             } 
         return count;
