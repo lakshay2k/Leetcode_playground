5b756243028287a73b1839015537c87da6322927
lakshay2k <lakshaykmsharma@gmail.com>
2021-05-07 10:58:05 +0530

    Check if N and its doubles exits in C++ - comments added

--- SortingAndSearching/program10.cpp
+++ SortingAndSearching/program10.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
+
         int i,j,len=0;
         for(i=0;i<arr.size();i++)
         {
             len=len+1;
         }
+
         for(i=0;i<len;i++)
         {
             for(j=0;j<len;j++)
@@ -21,7 +23,8 @@ public:
                 }
             }
         }
+
         return false;
     }
 };
-                   //arr[i] == 2*arr[j] || (arr[i]<=0 && 2*arr[j]<=0)
+                  
