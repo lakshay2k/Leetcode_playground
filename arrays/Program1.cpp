63f0351c596d9efa4b9a38515bb7b7ddca3a8a31
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-06-21 11:15:49 +0530

    Removing duplicate elements from sorted array -  improvised and commented

--- arrays/Program1.cpp
+++ arrays/Program1.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int i,j,len=0,to_delete;
-        for(i=0;i<nums.size();i++)
-        {
-            len=len+1;
-        }
+        
+        //taking len which we will decrement after every duplicaet removal
+        int i,j,len=nums.size(),to_delete;  //to_delete for storing index of no. to be deleted 
+        
+        //goinf till second last element
         for(i=0;i<len-1;i++)
         {
             if(nums[i] == nums[i+1])
             {
-                to_delete=i;
-                for(j=to_delete+1;j<nums.size();j++)
+                to_delete=i+1;
+                for(j=to_delete;j<len;j++)
                 {
                     nums[j-1]=nums[j];
                 }
-                len--;
-                i=i-1;
+                len--; // for updating len of array after duplicate removal
+                i=i-1; // for considering the number after duplicate to check if more duplicate of that no. exists or not.
             }
         }
+        
+        //returning length of updated array
         return len;
     }
 };
