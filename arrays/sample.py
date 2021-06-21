7223b7d8117ed1cab47d1a8085b5bc8a44f51d0e
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-06-21 12:22:28 +0530

    Buy and sell stock l in C++

---
+++ arrays/sample.py
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int s=prices.size(), profit= 0, min = prices[0];
+        for(int i=0;i<s;i++)
+        {
+            //if profit is less than the difference of sell price and buy price
+            if(profit < (prices[i]-min))
+            {
+                profit=prices[i]-min;
+            }
+            
+            //checking the min to buy the stock on that day
+            if(min>prices[i])
+                min=prices[i];
+        }
+        return profit;
+    }
+};
