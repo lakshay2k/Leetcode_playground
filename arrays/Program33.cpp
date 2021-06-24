4c7fa70f52aca132371f41aaeb079d796d682e1f
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-06-24 10:18:51 +0530

    Single Number lll in C++

--- arrays/Program33.cpp
+++ arrays/Program33.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
+        //creating the map for storing the element with its occurence
         unordered_map<int,int> mymap;
         for(int i=0;i<nums.size();i++)
         {
             mymap[nums[i]]++;
         }
+        
+        //adding the element whose occurences are 1 to the result vector and returning it.
         vector<int> res;
         for(auto x:mymap)
         {
