19fb6f2868e70c813ff0c4a3114fa9834956c45b
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-06-24 10:21:02 +0530

    Missing Number in C++ (easy level)

--- arrays/Program34.cpp
+++ arrays/Program34.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+        //reducing the space complexity
+        //calculating two sums and the difference comes the missing element
         int sum1 =0, sum2=0;
+        
+        //calculating the sum of series using formula
         sum1 = (nums.size()*(nums.size()+1))/2;
         for(auto i:nums)
         {
