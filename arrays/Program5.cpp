1f69cf3acca574a03c127a2120a237e1f7161ab6
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-06-24 10:13:42 +0530

    Single Number Problem in C++

--- arrays/Program5.cpp
+++ arrays/Program5.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int i;
-        for(i=0;i<nums.size();i++)
+        //sorting the whole array
+        sort(nums.begin(),nums.end());
+        
+        //checking every 2 elements to skip duplicates and adding check if we come to last element
+        for(int i=0;i<nums.size();i=i+2)
         {
-            int a=count(nums.begin(),nums.end(),nums[i]);
-            if(a!=2)
+            if(nums[i]!=nums[nums.size()-1])
             {
+            if(nums[i]!=nums[i+1])
                 return nums[i];
             }
+            else
+                return nums[i];
         }
         return 0;
     }
