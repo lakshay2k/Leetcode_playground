19b3322ee483501b4c7adfa3ac76705ae5614922
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-07-18 11:58:04 +0530

    Pascal's Traingle ll in C++ using DP - comments added

--- dynamic_programming/Program15.cpp
+++ dynamic_programming/Program15.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
+        
+        //creating a vector to store the pascal triangle pattern
         vector<int> pascal(rowIndex+1,1);
+        
         long int res=1;
         int n=rowIndex;
+        
+        //the formula that can be used to get the nth row of pascals triangle is that
+        // n-1*C*c-1  -> small c goes to 0(keeps on decreasing) 
         for(int i=1;i<rowIndex;i++)
         {
             /*
