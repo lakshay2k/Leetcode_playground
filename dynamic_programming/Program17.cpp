f3c34e8a54953953b15de364c95b0ec23528afed
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-07-20 09:48:27 +0530

    Counting bits - comments added

--- dynamic_programming/Program17.cpp
+++ dynamic_programming/Program17.cpp
@@ -2,8 +2,22 @@ class Solution {
 public:
     vector<int> countBits(int n) {
         
+        //creating a vector of n+1 size and initializing all to zero
         vector<int> ar(n+1,0);
         //ar[0]=0;
+        /*
+Input: n = 5
+Output: [0,1,1,2,1,2]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+3 --> 11
+4 --> 100
+5 --> 101
+        */
+        //if we observe clearly, number at even places has same number of ones at their index/2
+        //for the odd it is just one more than half of index element
         for(int i=1;i<=n;i++)
         {
             if(i%2 == 0)
