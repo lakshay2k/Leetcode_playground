5e1f52978daab68ee495f967e4ee077bd1b0e76e
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-07-22 09:43:19 +0530

    Pyramid pattern using recursive calls in C++

---
+++ Recursion/Program2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void printpattern(int n, int j)
+{
+    if(n==0)
+    return;
+    if(j==0)
+    {
+        //cout<<"*";
+        printpattern(n-1,0);
+        cout<<endl;
+    }
+    if(j<n)
+    {
+    cout<<"*";
+    printpattern(n,j+1);
+    //cout<<n<<endl;
+    }
+}
+
+int main()
+{
+    //cout<<"Hello World";
+    printpattern(5,0);
+
+    return 0;
+}
