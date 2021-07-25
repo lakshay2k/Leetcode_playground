e17d83205250c6c9a2f5db56bd5b61cee3bfa1a0
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-07-25 09:33:28 +0530

    Remove the Element at Nth node of linked List

--- linkedlist/Program2.cpp
+++ linkedlist/Program2.cpp
@@ -14,12 +14,14 @@ public:
         int len=0,i;
         struct ListNode* temp=head;
         
-        //counting total number of nodes in linked list
+        //counting total number of nodes in linked list - length of linkedlist
         while(temp!=NULL)
         {
             temp=temp->next;
             len++;
         }
+        
+        //if length of node is n the it is the head which is to be deleted
         if(len==n)
         {
             ListNode* newhead = head->next;
@@ -27,12 +29,14 @@ public:
             return newhead;
         }
             
-            
+        //iterating to that list
         temp=head;
         for(i=1;i<(len-n);i++)
         {
             temp=temp->next;
         }
+        
+        //will make the pointers pointed to the next of node to be deleted
         ListNode* to_delete=temp->next;
         temp->next=temp->next->next;
         delete to_delete;
