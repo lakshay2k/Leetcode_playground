9bc0533e3a654ae2a60f6f61a2c09171259933ae
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-07-25 09:40:31 +0530

    How to reverse a Linkedlist in C++ - comments added

--- linkedlist/Program3.cpp
+++ linkedlist/Program3.cpp
@@ -11,14 +11,25 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
+        
+        //We will be using three pointers to keep track of Previous, Current and Next node
        ListNode *prevnode=NULL, *currnode=head, *nextnode=head;
+        
+        //iterate till the next node becomes null
         while(nextnode!=NULL)
         {
+            /*
+            first we move the next node ahead 
+            change the next of current node to prev (as we want to reverse the linkedlist)
+            now the previous node becomes the current node
+            */
             nextnode=nextnode->next;
             currnode->next=prevnode;
             prevnode=currnode;
             currnode=nextnode;
         }
+        
+        //Our head will the pointer where previous node is pointing
         head=prevnode;
         return head;
     }
