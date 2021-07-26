bd63217c2d91d3e3f63260629bcf32ab3acfdc55
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-07-26 09:47:18 +0530

    Increasing and Decreasing Pattern - comments added

--- Recursion/Program4.cpp
+++ Recursion/Program4.cpp
@@ -3,14 +3,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//recursive function 
 void incrdecr(int n)
 {
+    //if we get till 1 simply print 1;
     if(n==1)
     {
         cout<<1;
         return;
     }
-
+    
+    //For printing the decreasing pattern we will call print before the recursive call
+    //and for the increasing pattern, we will call print after the recursive call
     cout<<n<<endl;
     incrdecr(n-1);
     cout<<n<<endl;
@@ -18,6 +22,7 @@ void incrdecr(int n)
 
 int main()
 {
+    //calling from the main function
     incrdecr(5);
     return 0;
 }
