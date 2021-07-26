83ae7176729488f479715a77b0b991b9c6824089
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-07-26 10:49:26 +0530

    Print Linkedlist  - Question link added

--- linkedlist/Program12.cpp
+++ linkedlist/Program12.cpp
@@ -1,3 +1,6 @@
+//Question is to print the linkedlist having check if list is empty or not
+// Question link: https://practice.geeksforgeeks.org/problems/print-linked-list-elements/1
+
 class Solution
 {
     public:
