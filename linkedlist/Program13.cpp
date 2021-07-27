e4895e898822d048f8133f9ced2c33885678725b
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-07-27 09:31:55 +0530

    Length of linkedlist - comments added

--- linkedlist/Program13.cpp
+++ linkedlist/Program13.cpp
@@ -7,14 +7,22 @@ class Solution
     public:
     //Function to count nodes of a linked list.
     int getCount(struct Node* head){
+        
+        //initialising the count variable
         int count=0;
+        
+        //if the linkedlist is null then length is zero (0)
         if(head==NULL)
         return 0;
+        
+        //Iterate to the end of linkedlist and keep counting the nodes
         while(head!=NULL)
         {
             count=count+1;
             head=head->next;
         }
+        
+        //return the length of linkedlist OR count
         return count;
     
     }
