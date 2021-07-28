11f6b2dd9d0c301b63500adc1d4f69880e1ac3e4
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-07-28 10:16:25 +0530

    Get Nth node in a linkedlist - Question link added

--- linkedlist/Program14.cpp
+++ linkedlist/Program14.cpp
@@ -1,5 +1,11 @@
+// Question :Create a GetNth for taking the value at a given index of a linkedlist
+//Question link: https://practice.geeksforgeeks.org/problems/node-at-a-given-index-in-linked-list/1
+
+
 int GetNth(struct node* head, int index){
-  // Code here
+  // Assuming that Linkedlist is 1-indexed
+  
+
   if(index==1)
   return head->data;
   while(index!=1)
