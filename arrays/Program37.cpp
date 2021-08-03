ac4604a16bc18c5461a31dc6c910b72d9db17e92
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-08-03 09:42:12 +0530

    Sum of all prime numbers between 1 to N - comments added

--- arrays/Program37.cpp
+++ arrays/Program37.cpp
@@ -5,11 +5,15 @@
 class Solution{
 	public:
    	long long int prime_Sum(int n){
-   	    long long int sum = 0;
-   	    vector<bool> check(n+1,true);
+		
+		//using long long becuase the number can be large enough so that 
+   	    long long int sum = 0;  // sum initially zero to add up the numbers
+   	    vector<bool> check(n+1,true);  // array that will keep a check on prime numbers(true) otherwise false
         
+		//the square of a number is within the range then we can take it otherwise not.
         for(int p=2;p*p<=n;p++)
         {
+		//we make all the factors of that numbers as false
             if(check[p])
             {
                 for(int i=p*p;i<=n;i=i+p)
@@ -18,7 +22,9 @@ class Solution{
                 }
             }
         }
+		//at the end we are left with true ones and they are prime numbers
         
+		//summing all the true(prime) numbers 
         for(int p=2;p<=n;p++)
         {
             if(check[p])
