e759ab35365cd08fee7ed8bdee6f64bc0cd01d6f
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-08-08 22:49:03 +0530

    Butterfly pattern in C++

---
+++ Design/Program4.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int row =4;
+    for(int i=1;i<=row;i++)
+    {
+        for(int j=1;j<=2*row;j++)
+        {
+            if(j>i && j<=(2*row-2*i)+i)
+            cout<<"  ";
+            else
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    for(int i=row;i>=1;i--)
+    {
+        for(int j=1;j<=2*row;j++)
+        {
+            if(j>i && j<=(2*row-2*i)+i)
+            cout<<"  ";
+            else
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
