e02204222a36cef8e40b6e079f865dfb35067568
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-08-09 21:13:07 +0530

    Rhombus Pattern in C++

---
+++ Design/Program5.cpp
@@ -0,0 +1,29 @@
+//# palindromic pattern of numbers
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //cout<<"Hello World";
+    
+    int row = 5;
+    for(int i=1;i<=row;i++)
+    {
+        
+        for(int j=1;j<=row;j++)
+        {
+            if(j<=row-i)
+            cout<<"  ";
+        }
+        for(int k=1;k<=row;k++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    
+
+    return 0;
+}
