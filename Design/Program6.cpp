c0a50ed5a9b25df9e46a183c8420cb6e5c046e22
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-08-13 09:40:35 +0530

    Palindromic pattern of numbers - comments added

--- Design/Program6.cpp
+++ Design/Program6.cpp
@@ -23,14 +23,18 @@ int main()
     int row = 5;
     for(int i=1;i<=row;i++)
     {
+            //we would be dividing the pattern into two halves-> left and right
         int k=i;
         for(int j=1;j<=row;j++)
         {
+                //this if is for printing the spaces
             if(j<=row-i)
             cout<<"  ";
+                //this is for printing the left side pattern of decreasing numbers
             else
             cout<<k--<<" ";
         }
+            //this is for priniting the right side of pattern starting with 2
         if(i>1)
         {
             for(int l=2;l<=i;l++)
