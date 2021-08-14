0e333fe128748e44e0600636d4367f8054a8d71b
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-08-14 09:11:36 +0530

    Star Pattern in C++

---
+++ Design/Program7.cpp
@@ -0,0 +1,53 @@
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //cout<<"Hello World";
+    
+    int row = 5;
+    for(int i=1;i<=row;i++)
+    {
+        int k=i;
+        for(int j=1;j<=row;j++)
+        {
+            if(j<=row-i)
+            cout<<"  ";
+            else
+            cout<<"* ";
+        }
+        if(i>1)
+        {
+            for(int l=2;l<=i;l++)
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    
+    for(int i=row;i>=1;i--)
+    {
+        int k=i;
+        for(int j=1;j<=row;j++)
+        {
+            if(j<=row-i)
+            cout<<"  ";
+            else
+            cout<<"* ";
+        }
+        if(i>1)
+        {
+            for(int l=2;l<=i;l++)
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    
+    
+
+    return 0;
+}
+
