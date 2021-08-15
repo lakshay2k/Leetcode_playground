e220618cd9cf97a13a79c4594f54bd83fbad5420
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-08-15 10:11:39 +0530

    Pattern of numbers - comments added

--- Design/Program8.cpp
+++ Design/Program8.cpp
@@ -11,13 +11,16 @@ int main()
     12345
     */
     
+    //number of rows and columns is 5
     int rows = 5;
     for(int i=1;i<=5;i++)
     {
+        //used for printing the number till the value of i
         for(int j=1;j<=i;j++)
         {
             cout<<j;
         }
+        //after every new row, we add a new line to it.
         cout<<endl;
     }
     return 0;
