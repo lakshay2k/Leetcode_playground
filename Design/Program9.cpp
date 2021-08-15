ce03f19c7d26e1cf89d7826ec894f1c838b286b9
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-08-15 10:15:58 +0530

    Pattern of numbers 2 - comments added

--- Design/Program9.cpp
+++ Design/Program9.cpp
@@ -10,14 +10,17 @@ int main()
 4444
 55555
 */
-    
+    // number of rows and columns is 5
     int rows = 5;
     for(int i=1;i<=5;i++)
     {
+        //this is for times the numbers are printing in a row
         for(int j=1;j<=i;j++)
         {
+            //we will be printing i value j times as per question 
             cout<<i;
         }
+        //we add a new line after each row
         cout<<endl;
     }
     return 0;
