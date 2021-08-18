c0b320eee80fda902c120227767cfd08e4edcb7e
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-08-18 09:37:25 +0530

    Length of Cycle in Linkedlist

---
+++ linkedlist/Program18.cpp
@@ -0,0 +1,28 @@
+int countNodesinLoop(struct Node *head)
+{
+    // Code here
+    int count=0;
+    Node* fast = head->next;
+    Node* slow = head;
+    
+    while(fast!=slow && (fast!=NULL && fast->next!=NULL))
+    {
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    if(fast!=NULL && fast->next!=NULL)
+    {
+    slow=slow->next;
+    count=count+1;
+    while(slow!=fast)
+    {
+        count=count+1;
+        slow=slow->next;
+    }
+    return count;
+    }
+    else
+    {
+        return 0;
+    }
+}
