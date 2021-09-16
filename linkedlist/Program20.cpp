abcf6a005f3b64aad82d04dad95c06e953d60ada
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-09-16 10:01:46 +0530

    Delete N nodes after M nodes of LinkedList

--- linkedlist/Program20.cpp
+++ linkedlist/Program20.cpp
@@ -10,24 +10,33 @@ class Solution
         struct Node *curr = head;
         struct Node *t;
         
+        //iterate till we reach the last node
         while(curr)
         {
+            //count the m nodes
             for(int count = 1;count<M &&curr!=NULL;count++)
             {
                 curr=curr->next;
             }
+            //if after counting m nodes, we reach the last node then just return
             if(curr == NULL)
             return;
             
+            //from the next node, we start to delete the N nodes
             t = curr->next;
             
+            //deleting N nodes
             for(int count = 1;count<=N && t!=NULL;count++)
             {
                 struct Node *temp = t;
                 t=t->next;
                 free(temp);
             }
+            
+            //connecting the links after deleting N nodes
             curr->next = t;
+            
+            //to return the curr pointer.
             curr = t;
         }
         
