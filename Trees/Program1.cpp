525eaacf68cf1f77985cbe9603985bb4e9c47224
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-09-17 10:31:34 +0530

    Height of Binary Tree

--- Trees/Program1.cpp
+++ Trees/Program1.cpp
@@ -4,6 +4,15 @@
 class Solution{
     public:
     
+    //Function to find the height of a binary tree.
+    /*
+    we will be using DFS - Depth First Search
+    people might consider nodes from root node to leaf node
+    Height of binary tree = number of edges from the root node to the leaf node.
+    heighht of a x node = number of edges from x node to the leaf node.
+    height of binary tree  = height of root node.
+    */
+    
     int height(struct Node* node){
         // code here 
         //base condition
