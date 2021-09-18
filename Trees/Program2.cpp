6de75a096fb85c61859e83ff538ba6b7fe0241a9
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-09-18 09:39:14 +0530

    Level Order Traversal of Binary Tree

--- Trees/Program2.cpp
+++ Trees/Program2.cpp
@@ -12,8 +12,11 @@ class Solution
       if(node == NULL)
       return res;
       
+        //we will be using queue to store the children of already discovered node
       queue<Node *> q;
       q.push(node);
+        
+        //iterate till the queue is not empty
       while(q.empty() == false)
       {
           Node* temp=q.front();
@@ -21,8 +24,12 @@ class Solution
           
           res.push_back(temp->data);
           q.pop();
+          
+          //after each dequeue from queue, we enqueue the children of node removed.
+          //after checking if left or right child is present, then we enqueue it.
           if(temp->left !=NULL)
           q.push(temp->left);
+          
           if(temp->right != NULL)
           q.push(temp->right);
           
