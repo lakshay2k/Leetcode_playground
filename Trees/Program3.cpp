8355094e105c2b8ec30cb458733c1e22073c06c5
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-09-19 11:53:31 +0530

    InOrder Traversal of Binary Tree

---
+++ Trees/Program3.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    // Function to return a list containing the inorder traversal of the tree.
+    vector<int> res;
+    vector<int> inOrder(Node* root) {
+        // Your code here
+        if(root == NULL)
+        return res;
+        inOrder(root->left);
+        res.push_back(root->data);
+        inOrder(root->right);
+        return res;
+    }
+};
