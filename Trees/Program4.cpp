a02962eaba18866c159c671b62f3b7611e859916
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-09-20 10:17:10 +0530

    Preorder Traversal of Binary Tree

--- Trees/Program4.cpp
+++ Trees/Program4.cpp
@@ -5,18 +5,23 @@
 Class Solution
 {
 public:
-      vector<int> res;
+      vector<int> res; //global vector to store the pattern of traversal
+      
 vector <int> preorder(Node* root)
 {
     //vector<int> res;
   // Your code here
+  //if root does not point to NULL then only we will proceed.
   if(root!=NULL)
   {
+  
+  //Preorder : Root-> LeftNode -> RightNode
   res.push_back(root->data);
   //cout<<root->data;
-  preorder(root->left);
-  preorder(root->right);
+  preorder(root->left);  //left subtree
+  preorder(root->right); //right subtree
   return res;
+  
   }
   return res;
 }
