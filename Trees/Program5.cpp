8b026e0cda2be5b991c79c5848be4b9f7564b0fb
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-09-21 10:29:34 +0530

    PostOrder Traversal in a Binary Tree

--- Trees/Program5.cpp
+++ Trees/Program5.cpp
@@ -3,17 +3,22 @@
 
 Class Solution
 {
+//global vector to store the traversal 
 vector<int> res;
+
 vector <int> postOrder(Node* root)
 {
   // Your code here
+  //if tree is empty then just return
   if(root == NULL)
   return res;
   
   //cout<<root->data;
+  //PostOrder : leftNode->RightNode->Root
   postOrder(root->left);
   postOrder(root->right);
   res.push_back(root->data);
+  
   return res;
 }
 }
