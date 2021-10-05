87d56f3d546f3d93c834e576a38c914f3d101029
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-10-05 10:04:00 +0530

    Print Pattern in C++

--- maths/Program7.cpp
+++ maths/Program7.cpp
@@ -5,10 +5,14 @@ void printPat(int n)
 {
 //Your code here
 int a=n;
+    //Keep decreamenting a to keep count of dollar at each newline
 while(a>0)
 {
+    //we have to print values of integers N times hence, we will be using two loops
+    //1. for keeping N rows
 for(int i=n;i>0;i--)
 {
+    //2. for printing numbers N times
     for(int j=a;j>0;j--)
     {
         cout<<i<<" ";
