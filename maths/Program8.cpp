2f1b99c68f15aaeb818bda54547586b11e3e7617
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-10-06 09:11:34 +0530

    Nth Term of AP in C++

--- maths/Program8.cpp
+++ maths/Program8.cpp
@@ -5,8 +5,12 @@ class Solution {
   public:
     int nthTermOfAP(int A1, int A2, int N) {
         // code here
+      //calculating the difference of terms (d)
         int d = A2 - A1;
+      
+      //Formula is An = A + (n-1)*d
         int res = A1 + (N-1)*d;
+      //returning the result
         return res;
     }
 };
