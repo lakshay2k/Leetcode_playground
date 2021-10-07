271441f454b44a7c1f2910e6ec51adc1e1ba752e
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-10-07 10:22:57 +0530

    Nth Term of a GP in C++

---
+++ maths/Program9.cpp
@@ -0,0 +1,16 @@
+class Solution{
+	public:
+   	int Nth_term(int a, int r, int n){
+   	    // Code here
+   	    int x=1;
+   	    for(int i=1;i<n;i++)
+   	    {
+   	        x=(x*r)%1000000007;
+   	    }
+   	    cout<<"1: "<<x<<endl;
+   	    
+   	    x = (x*a)%1000000007;
+   	    cout<<"2: "<<x;
+   	    return x;
+   	}    
+};
