92aef1a9ee30b43e9faa0e7705454f18d4b81927
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-10-08 09:51:58 +0530

    Closest Number in C++

---
+++ maths/Program10.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int closestNumber(int N , int M) {
+        
+        int quotient = N/M;
+        int root1 = quotient*M;
+        int root2;
+        if(N*M > 0)
+        root2 = M*(quotient + 1);
+        else
+        root2 = M*(quotient - 1);
+        
+        if(abs(N-root1)<abs(N-root2))
+        return root1;
+        else 
+        return root2;
+    }
+};
