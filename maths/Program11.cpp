40f9f7b21b27803da8b8484e2719bf55955dd822
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-10-09 09:39:01 +0530

    Update Program11.cpp

--- maths/Program11.cpp
+++ maths/Program11.cpp
@@ -6,13 +6,16 @@ class Solution {
   public:
     string armstrongNumber(int n){
         // code here
+      //taking variables to store the rem, quotient and sum.
         int rem=0,sum=0,temp=n;
         while(temp)
         {
             rem=temp%10;
+          //armstrong number has the sum of cubes of every digit equal to the number itself.
             sum = sum +rem*rem*rem;
             temp=temp/10;
         }
+      //if the condition is satisfied then it is armstrong number otherwise not.
         if(sum == n)
         return "Yes";
         else
