0053eaf89a75464411db7e507daf9c6ccaffb83b
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-10-09 09:36:27 +0530

    Armstrong Numbers in C++

---
+++ maths/Program11.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    string armstrongNumber(int n){
+        // code here
+        int rem=0,sum=0,temp=n;
+        while(temp)
+        {
+            rem=temp%10;
+            sum = sum +rem*rem*rem;
+            temp=temp/10;
+        }
+        if(sum == n)
+        return "Yes";
+        else
+        return "No";
+    }
+};
