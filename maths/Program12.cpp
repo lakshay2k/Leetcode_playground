9773cbaade832d1da99e0c29c642679675eff8af
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-10-11 10:03:06 +0530

    Sum of Digits is palindrome or not

---
+++ maths/Program12.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    int isDigitSumPalindrome(int N) {
+        // code here
+        int sum=0,sum2=0;
+        int rem;
+        int temp=N;
+        while(temp)
+        {
+            rem = temp%10;
+            sum=sum+rem;
+            temp=temp/10;
+        }
+        temp=sum;
+        while(temp)
+        {
+            rem = temp%10;
+            sum2=sum2*10+rem;
+            temp=temp/10;
+        }
+        if(sum2 == sum)
+        return 1;
+        else
+        return 0;
+    }
+};
