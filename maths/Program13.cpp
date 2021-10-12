a0752d24d0ff4e923b3c224049104ec0eb0c3597
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-10-12 09:07:48 +0530

    Perfect Numbers in

---
+++ maths/Program13.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int isPerfectNumber(long long N) {
+        // code here
+        long long sum=1;
+        for(long long i=1;i<sqrt(N);i++)
+        {
+            if(N%i==0)
+            {
+                if(N/i == i)
+                sum=sum+i;
+                else
+                sum = sum + i + N/i;
+            
+            }
+        }
+        if(sum == N)
+        return 1;
+        return 0;
+    }
+};
