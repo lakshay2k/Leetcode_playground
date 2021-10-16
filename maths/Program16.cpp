8d953c99bcba89e7a2b1ba907d817e7e226ede2f
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-10-16 09:33:34 +0530

    Reverse the digits

---
+++ maths/Program16.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+	public:
+	
+		long long int reverse_digit(long long int n)
+		{
+		    // Code here
+		    long long int rem,temp,sum=0;
+		    temp=n;
+		    while(temp)
+		    {
+		        rem=temp%10;
+		        sum = sum*10+rem;
+		        temp=temp/10;
+		    }
+		    return sum;
+		}
+};
