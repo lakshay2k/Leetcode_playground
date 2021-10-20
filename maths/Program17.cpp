109ba27e651351f1e0df8f1a76b786db1e5af205
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-10-20 09:40:49 +0530

    Print the Kth Digit

--- maths/Program17.cpp
+++ maths/Program17.cpp
@@ -5,7 +5,7 @@ class Solution{
 public:
     
     int kthDigit(int A,int B,int K){
-        // code here
+        // declaring long int to store the large number after power operation
         long long int num = pow(A,B);
         //cout<<num<<endl;
         int rem=0;
