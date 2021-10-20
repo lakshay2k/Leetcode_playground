e3414ead5e464fa04a1c4561d8374b1a20833f9a
Lakshay Sharma <43582556+lakshay2k@users.noreply.github.com>
2021-10-20 09:22:07 +0530

    Print the Kth Digit

---
+++ maths/Program17.cpp
@@ -0,0 +1,17 @@
+class Solution{
+public:
+    int kthDigit(int A,int B,int K){
+        // code here
+        long long int num = pow(A,B);
+        //cout<<num<<endl;
+        int rem=0;
+        while(K!=0 && num!=0)
+        {
+            rem=num%10;
+            num=num/10;
+            K--;
+          //  cout<<rem<<" "<<num<<endl;
+        }
+        return rem;
+    }
+};
